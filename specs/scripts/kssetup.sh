f84074ad830381b947c3da94482015bb04b86cc2
Rob Lyon <nosignsoflifehere@gmail.com>
2014-03-28 10:50:50 -0700

    addressing travis errors in ksvalidator setup

--- specs/scripts/kssetup.sh
+++ specs/scripts/kssetup.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-if ! [ "x${TRAVIS}" == "x" ] ; then
-  sudo pip install virtualenv
+echo "Starting setup"
+if [ "${TRAVIS}" == "true" ] ; then
+  sudo ip install pycurl
+  sudo pip install urlgrabber
+  git clone git://git.fedorahosted.org/git/pykickstart.git
+  cd pykickstart
+  sudo python setup.py install
 else
   pip install virtualenv
+  echo "Creating virtual environment: ksvalidator"
+  virtualenv ksvalidator
+  cd ksvalidator
+  echo "Activating!!!"
+  source bin/activate
+  echo "Installing packages"
+  pip install pycurl
+  pip install urlgrabber
+  git clone git://git.fedorahosted.org/git/pykickstart.git
+  cd pykickstart
+  python setup.py install
 fi
-
-echo "Creating virtual environment: ksvalidator"
-virtualenv ksvalidator
-cd ksvalidator
-echo "Activating!!!"
-source bin/activate
-
-echo "Installing packages"
-pip install pycurl
-pip install urlgrabber
-git clone git://git.fedorahosted.org/git/pykickstart.git
-cd pykickstart
-python setup.py install
-
 echo "Setup complete"
